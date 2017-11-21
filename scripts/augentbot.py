a4be95199884c0efae2fc496410589815a56034f
jfde <31999724+jfde@users.noreply.github.com>
2017-11-21 22:30:53 +0100

    Move runtime constants & files to own namespace Runtime constants and files are now in their own file, creating a new namespace 'augent_constants'. Files that are read from can be re-read with 'augent_constants.read_{filename}()'

--- scripts/augentbot.py
+++ scripts/augentbot.py
@@ -1,42 +1,13 @@
-#! python3
+#! python3.6
 
-import _io
 import datetime
-import os
 import platform
-from typing import Optional, Iterable, Union
+from typing import List, Union
 
-import tweepy
-from pymarkovchain_dynamic import MarkovChain, DynamicMarkovChain
-
+import augent_constants
 import timestamps
 import tweet_text
-
-TWITTER_CONSUMER_KEY = open(os.path.join(os.path.expanduser('~'), 'augentbot', 'credentials',
-                                         'twitter_consumer_key')).read()
-TWITTER_CONSUMER_SECRET = open(os.path.join(os.path.expanduser('~'), 'augentbot', 'credentials',
-                                            'twitter_consumer_secret')).read()
-TWITTER_ACCESS_TOKEN = open(os.path.join(os.path.expanduser('~'), 'augentbot', 'credentials',
-                                         'twitter_access_token')).read()
-TWITTER_ACCESS_TOKEN_SECRET = open(os.path.join(os.path.expanduser('~'), 'augentbot', 'credentials',
-                                                'twitter_access_token_secret')).read()
-
-HOST_NAME = '_jfde'
-
-DATA = os.path.join(os.path.expanduser('~'), 'augentbot', 'data')
-
-auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth)
-
-
-def confirm(prompt: str = 'Confirm this action?') -> bool:
-    prompt = prompt.strip()
-    if not prompt.endswith('?'):
-        prompt += '?'
-    prompt += ' (y/n): '
-    return input(prompt).lower().strip() == 'y'
+from pymarkovchain_dynamic import MarkovChain, DynamicMarkovChain
 
 
 def notify_me(text: str) -> None:
@@ -46,15 +17,12 @@ def notify_me(text: str) -> None:
     """
     for subtext in [text[i:i+10000] for i in range(0, len(text), 10000)]:
         try:
-            api.send_direct_message(screen_name=HOST_NAME, text=subtext)
-        except tweepy.TweepError as e:
+            augent_constants.api.send_direct_message(screen_name=augent_constants.HOST_NAME, text=subtext)
+        except augent_constants.tweepy.TweepError as e:
             log_info("{0} when trying to send the following dm:\n    '{1}'".format(e, text))
 
 
-def log_info(entry: str,
-             notify: bool = False,
-             file: Optional[_io.TextIOWrapper] = None,
-             close_file: bool = True) -> None:
+def log_info(entry: str, notify: bool = False) -> None:
     """
     Attaches a timestamp with the current time to the entry,
     prints the entry and saves it in the log.txt file of the data directory.
@@ -62,43 +30,39 @@ def log_info(entry: str,
     user specified as HOST_NAME via twitter dm. This requires that the user
     has allowed receiving dms from this account
     """
-    if file is None:
-        file = open(os.path.join(DATA, "log.txt"), 'a', encoding='utf_16')
-    
-    file.write(timestamps.add_timestamp(entry) + '\n')
+    augent_constants.log_file.write(timestamps.add_timestamp(entry) + '\n')
     print(entry)
     if notify:
         notify_me(entry)
-    
-    if close_file:
-        file.close()
 
 
 def followback() -> None:
-    followers = [follower.screen_name for follower in tweepy.Cursor(api.followers).items()]
+    followers = [follower.screen_name for follower in
+                 augent_constants.tweepy.Cursor(augent_constants.api.followers).items()]
     # follow back
-    followings = [following.screen_name for following in tweepy.Cursor(api.friends).items()]
+    followings = [following.screen_name for following in
+                  augent_constants.tweepy.Cursor(augent_constants.api.friends).items()]
     for follower in followers:
-        if follower not in followings + tweet_text.IGNORED_USERS:
+        if follower not in followings + augent_constants.IGNORED_USERS:
             try:
-                api.create_friendship(follower)
+                augent_constants.api.create_friendship(follower)
                 log_info('followed @{0}'.format(follower))
-            except tweepy.RateLimitError:
+            except augent_constants.tweepy.RateLimitError:
                 log_info('Rate limit exceeded.', True)
                 break
-            except tweepy.TweepError:
+            except augent_constants.tweepy.TweepError:
                 log_info("Couldn't follow @{0}".format(follower))
 
     # unfollow back
     for following in followings:
-        if following not in followers + tweet_text.IGNORED_USERS:
+        if following not in followers + augent_constants.IGNORED_USERS:
             try:
-                api.destroy_friendship(following)
+                augent_constants.api.destroy_friendship(following)
                 log_info('unfollowed @{0}'.format(following))
-            except tweepy.RateLimitError:
+            except augent_constants.tweepy.RateLimitError:
                 log_info('Rate limit exceeded.', True)
                 break
-            except tweepy.TweepError:
+            except augent_constants.tweepy.TweepError:
                 log_info("Couldn't follow @{0}".format(following))
 
 
@@ -109,52 +73,36 @@ def process_new_tweets() -> None:
     it is being added more often.
     If a tweet older than 7 days is encountered, the method is being returned.
     """
-    data_file = open(os.path.join(DATA, 'data.txt'), 'a', encoding='utf_16')
-    log_file = open(os.path.join(DATA, 'log.txt'), 'a', encoding='utf_16')
-    # don't open and close files for every data/logging entry
-
     def process_tweet(tweet):
         tweet_value = tweet_text.get_viable_text(tweet)
         if tweet_value:
-            log_info("Processing tweet {0}: '{1}' ... viable".format(tweet.author.screen_name, tweet_value),
-                     file=log_file, close_file=False)
+            log_info("Processing tweet {0}: '{1}' ... viable".format(tweet.author.screen_name, tweet_value))
             for i in range(tweet_text.get_weight(tweet)):
-                data_file.write(tweet_value)
+                augent_constants.data_file.write(tweet_value)
         else:
-            log_info("Processing tweet {0}: '{1}' ... not viable"
-                     .format(tweet.author.screen_name, tweet.text), file=log_file, close_file=False)
+            log_info("Processing tweet {0}: '{1}' ... not viable".format(tweet.author.screen_name, tweet.text))
 
-    for t in tweepy.Cursor(api.user_timeline, count=168).items():
+    for t in augent_constants.tweepy.Cursor(augent_constants.api.user_timeline, count=168).items():
         if t.created_at < datetime.datetime.now() - datetime.timedelta(days=7):
-            data_file.close()
-            log_file.close()
+            augent_constants.data_file.close()
+            augent_constants.log_file.close()
             return
         process_tweet(t)
-    data_file.close()
-    log_file.close()
+    augent_constants.data_file.close()
+    augent_constants.log_file.close()
     return
 
 
-def generate_tweets(count: int = 1, mc: Union[None, MarkovChain, DynamicMarkovChain] = None) -> Iterable[str]:
+def generate_tweets(count: int = 1, mc: Union[None, MarkovChain, DynamicMarkovChain] = None) -> List[str]:
     temporary_markov_chain = False
     if mc is None:
+        augent_constants.read_coll()
+        augent_constants.read_corpus()
         temporary_markov_chain = True
-        mc = MarkovChain()
+        mc: MarkovChain = MarkovChain()
+        mc.generateDatabase(augent_constants.corpus_data + augent_constants.coll_data, n=4)
 
-        # using a corpus of predefined data
-        with open(os.path.join(DATA, "corpus.txt"), encoding='utf_16') as file:
-            corpus_data = file.read()
-
-        # adding the collected data from other twitter users
-        with open(os.path.join(DATA, "data.txt"), encoding='utf_16') as file:
-            collected_data = file.read()
-
-        mc.generateDatabase(corpus_data+collected_data, n=4)
-
-        del corpus_data
-        del collected_data
-
-    tweets = []
+    tweets: augent_constants.List[str] = []
     for i in range(count):
         while True:
             tweet = tweet_text.make_tweet_text(mc.generateString())
@@ -193,29 +141,30 @@ def tweet_new(create_buffers: int = 0) -> None:
             tweets.append(t_text)
             # create a tweet and, if specified in function call, create additional tweets for the tweet buffer
 
-    api.update_status(tweets[0])
+    augent_constants.api.update_status(tweets[0])
     
     if create_buffers:
-        with open(os.path.join(DATA, 'buffer.txt'), 'a', encoding='utf_16') as file:
-            file.write('\n' + '\n'.join(tweets[1:]))
+        augent_constants.buffer_file.write('\n' + '\n'.join(tweets[1:]))
 
 
 def tweet_from_buffer() -> None:
-    # DynamicMarkovChain is not finished work yet. For now, there is no other way
-    # than tweeting from the pre-produced buffer
-    with open(os.path.join(DATA, 'buffer.txt'), encoding='utf_16') as file:
-        buffer = file.readlines()
-
-    api.update_status(buffer.pop())
-
-    with open(os.path.join(DATA, 'buffer.txt'), 'w', encoding='utf_16') as file:
-        file.write(''.join(buffer)[:-1])  # remove newline at end of file
+    augent_constants.read_buffer()
+    augent_constants.api.update_status(augent_constants.buffer_data.pop())
+    augent_constants.buffer_file.write(''.join(augent_constants.buffer_data)[:-1])  # remove newline at end of file
 
 
 def run(create_buffers: int = 0) -> None:
     try:
         followback()
+    except Exception as e:
+        log_info(str(e), notify=True)
+
+    try:
         process_new_tweets()
+    except Exception as e:
+        log_info(str(e), notify=True)
+
+    try:
         tweet_new(create_buffers)
     except Exception as e:
         log_info(str(e), notify=True)
@@ -224,8 +173,9 @@ def run(create_buffers: int = 0) -> None:
         except Exception as e:
             log_info('{} in buffer'.format(str(e)), notify=True)
 
-    
+
 if __name__ == '__main__':
+    import os
     if platform.system() == 'Windows':
         os.system('chcp 65001')  # fixes encoding errors on windows
 
